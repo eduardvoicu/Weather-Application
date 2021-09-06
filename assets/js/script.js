57637427ff399081fb3590ca9f9e7bf6fcbc72be
Eduard Voicu <iameduardvoicu@gmail.com>
2021-09-06 16:06:24 -0500

    creating function to initialize page

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,24 @@
+// Function for initiliazing the page with const variables
+function initPage() {
+    const inputEl = document.getElementById("city-input");
+    const searchEl = document.getElementById("search-button");
+    const clearEl = document.getElementById("clear-history");
+    const nameEl = document.getElementById("city-name");
+    const currentPicEl = document.getElementById("current-pic");
+    const currentTempEl = document.getElementById("temperature");
+    const currentHumidityEl = document.getElementById("humidity");4
+    const currentWindEl = document.getElementById("wind-speed");
+
+
+
+
+
+
+
+
+
+
+
 // Variables that will be used in the scripts below including API keys
 var key = "213cba83ade6abe9c240474e66a3a22f";
 
@@ -5,55 +26,55 @@ var key = "213cba83ade6abe9c240474e66a3a22f";
 
 
 
-// $("className).remove()
-$("#searchButton").click(function() {
-    city = $("#searchInput").val();
-    // fetchAPI();
+// // $("className).remove()
+// $("#searchButton").click(function() {
+//     city = $("#searchInput").val();
+//     // fetchAPI();
   
-    var checkArray = searchHistory.includes(city);
+//     var checkArray = searchHistory.includes(city);
   
-    if (checkArray == true) {
-        return;
-    } else {
-        searchHistory.push(city);
-        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+//     if (checkArray == true) {
+//         return;
+//     } else {
+//         searchHistory.push(city);
+//         localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
   
-        var historyButton = $("<a>").attr({
-            class: "list-group-item list-group-item-action",
-            href: "#",
+//         var historyButton = $("<a>").attr({
+//             class: "list-group-item list-group-item-action",
+//             href: "#",
   
-        });
-        historyButton.text(city);
-        $("#searchList").append(historyButton);
-    };
-  });
-  $(".list-group-item").click(function() {
-    city = $(this).text();
-    // fetchAPI();
-  });
+//         });
+//         historyButton.text(city);
+//         $("#searchList").append(historyButton);
+//     };
+//   });
+//   $(".list-group-item").click(function() {
+//     city = $(this).text();
+//     // fetchAPI();
+//   });
   
   
-  var searchHistory = [];
+//   var searchHistory = [];
   
-  function getItems() {
-      var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
-      if (storedHistory !== null) {
-          searchHistory = storedHistory;
-      };
+//   function getItems() {
+//       var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
+//       if (storedHistory !== null) {
+//           searchHistory = storedHistory;
+//       };
   
-      for (i = 0; i < searchHistory.length; i++) {
-          if (i == 8) {
-              break;
-            }
+//       for (i = 0; i < searchHistory.length; i++) {
+//           if (i == 8) {
+//               break;
+//             }
   
-            historyButton = $("<a>").attr({
-              class: "list-group-item list-group-item-action",
-              href: "#"
-          });
+//             historyButton = $("<a>").attr({
+//               class: "list-group-item list-group-item-action",
+//               href: "#"
+//           });
   
-          historyButton.text(searchHistory[i]);
-          $("#searchList").append(historyButton);
-      }
-  };
+//           historyButton.text(searchHistory[i]);
+//           $("#searchList").append(historyButton);
+//       }
+//   };
   
-  getItems();
+//   getItems();
