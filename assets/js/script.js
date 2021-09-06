f4fdcaebf2be1400a446829ce3b82f216de11e50
Eduard Voicu <iameduardvoicu@gmail.com>
2021-09-06 15:59:48 -0500

    Added html, script and styles. Created function for local storage and search history. and index

---
+++ assets/js/script.js
@@ -0,0 +1,59 @@
+// Variables that will be used in the scripts below including API keys
+var key = "213cba83ade6abe9c240474e66a3a22f";
+
+
+
+
+
+// $("className).remove()
+$("#searchButton").click(function() {
+    city = $("#searchInput").val();
+    // fetchAPI();
+  
+    var checkArray = searchHistory.includes(city);
+  
+    if (checkArray == true) {
+        return;
+    } else {
+        searchHistory.push(city);
+        localStorage.setItem("searchHistory", JSON.stringify(searchHistory));
+  
+        var historyButton = $("<a>").attr({
+            class: "list-group-item list-group-item-action",
+            href: "#",
+  
+        });
+        historyButton.text(city);
+        $("#searchList").append(historyButton);
+    };
+  });
+  $(".list-group-item").click(function() {
+    city = $(this).text();
+    // fetchAPI();
+  });
+  
+  
+  var searchHistory = [];
+  
+  function getItems() {
+      var storedHistory = JSON.parse(localStorage.getItem("searchHistory"));
+      if (storedHistory !== null) {
+          searchHistory = storedHistory;
+      };
+  
+      for (i = 0; i < searchHistory.length; i++) {
+          if (i == 8) {
+              break;
+            }
+  
+            historyButton = $("<a>").attr({
+              class: "list-group-item list-group-item-action",
+              href: "#"
+          });
+  
+          historyButton.text(searchHistory[i]);
+          $("#searchList").append(historyButton);
+      }
+  };
+  
+  getItems();
